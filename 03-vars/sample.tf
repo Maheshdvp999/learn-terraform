68d857a8d14b8b4be532c6eb0414c94c140afa69
mahes <maheshdvp999@gmail.com>
2023-09-06 23:25:03 +0530

    ansible

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -1,6 +1,35 @@
+
+#plain variables
 variable "fruit_name" {
   default = "apple"
 }
 output "fruit_name" {
   value = var.fruit_name
+}
+
+variable "fruits" {
+  default = ["apple","banana"]
+}
+
+#Map variable, plain
+
+variable "fruit_stock" {
+  default = {
+    apple= 300
+    banana= 200
+  }
+}
+
+#map variable map of maps
+variable "fruit_stock_with_price" {
+  default = {
+    apple ={
+      stock = 100
+      price = 3
+    }
+    banana= {
+      stock = 400
+      price= 1
+    }
+  }
 }
