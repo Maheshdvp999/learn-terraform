602e6ab99128bb9c709bb652abdbd8deabc14e23
mahes <maheshdvp999@gmail.com>
2023-09-07 18:32:10 +0530

    ansible

--- 03-vars/sample.tf
+++ 03-vars/sample.tf
@@ -42,3 +42,34 @@ output "fruits_first" {
 output "fruits_second" {
   value = "var.fruits[1]"
 }
+
+#access map variable
+output "fruit_stock_apple" {
+  value = var.fruit_stock[apple]
+}
+
+output "fruit_stock_with_price_of_apple" {
+  value = var.fruit_stock_with_price["apple"].stock
+}
+
+#variable data types
+variable "fruit_details" {
+  default = {
+    apple= {
+      stock = 100 #number
+      price= 1
+      type = "washington" #type_string
+      for_sale= true #boolean
+    }
+  }
+}
+
+#variable in a combination of any other string then it should in ${}
+output "fruit_name_1" {
+  value = "Fruit Name = ${var.fruit_name}"
+}
+
+output "fruit_details_apple" {
+  value = "Apple stock=${var.fruit_details["apple"].stock} , Apple Type = ${var.fruit_details["apple"]} , Apple sale status= ${var.fruit_details["apple"].for_sale}"
+
+}
