a5d35c010ae6fa85b3d0f177ab89ad6bdc9700d8
mahes <maheshdvp999@gmail.com>
2023-09-11 08:50:04 +0530

    ansible

---
+++ for-each/main.tf
@@ -0,0 +1,12 @@
+variable "components" {
+  default = {
+    catalogue= { name = 'catalogue_sg'}
+    mongodb= { name = "mongodb_sg" }
+  }
+}
+
+
+resource "aws_security_group" "allow-tls" {
+  for_each = var.components
+  name = lookup (var.components,each.value["name"], null)
+}
