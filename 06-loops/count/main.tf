e239a47ff95efc20a4cbbdd0b16adc0ee0c929b3
mahes <maheshdvp999@gmail.com>
2023-09-11 08:16:39 +0530

    ansible

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,14 @@
+variable "components" {
+  default = ["frontend", "mongodb"]
+}
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t3.micro"
+  vpc_security_group_ids=["sg-03a191c8c20494101"]
+
+  tags = {
+    Name = "frontend"
+  }
+}
